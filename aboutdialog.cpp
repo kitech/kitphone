bd0a88e4ab981175d3820660f9688f3edebe8831
liuguangzhao <liuguangzhao@users.sf.net>
2011-05-19 09:07:37 +0800

    jjjjjjjjjjjjj

---
+++ aboutdialog.cpp
@@ -0,0 +1,26 @@
+// aboutdialog.cpp --- 
+// 
+// Author: liuguangzhao
+// Copyright (C) 2007-2012 liuguangzhao@users.sf.net
+// URL: 
+// Created: 2011-05-18 22:30:44 +0800
+// Version: $Id$
+// 
+
+
+#include "ui_aboutdialog.h"
+#include "aboutdialog.h"
+
+AboutDialog::AboutDialog(QWidget *parent)
+    : QDialog(parent)
+    ,uiw(new Ui::AboutDialog())
+{
+    this->uiw->setupUi(this);
+    this->uiw->label_6->setText(KPVERSION_STR);
+}
+
+AboutDialog::~AboutDialog()
+{
+
+}
+
